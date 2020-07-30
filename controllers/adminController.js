fe8e0a4a77d0db9499772b2908fdc16e10831fd0
ahsantawil <ahsan.tawil07@gmail.com>
2020-07-30 12:13:44 +0700

    Crud and add function

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,14 +6,22 @@ module.exports = {
         res.render('admin/dashboard/view_dashboard');
     },
 
-    viewCategory : (req, res) => {
-        res.render('admin/category/view_category');
+    viewCategory : async (req, res) => {
+        const category = await Category.find();
+        res.render('admin/category/view_category', {category});
     },
 
     addCategory : async (req,res) => {
         const {name} = req.body;
-        // console.log(name);
         await Category.create({name});
+        res.redirect('/admin/category');
+    },
+    
+    editCategory : async (req, res) => {
+        const{id, name} = req.body;
+        const category  = await Category.findOne({_id:id});
+        category.name = name;
+        await category.save();
         res.redirect('/admin/category');
     },
 
